45d9334ba74932d71daba3f9f31a8e593d1d3eb5
Manuel Lucas <manuel.lucas@Nology.local>
2020-11-19 10:58:14 +0000

    added dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Routes from "./containers/Routes";
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Routes.module.scss";
+import { Router } from "@reach/router";
+
+const Routes = () => {
+  return (
+    <>
+      {/* <Router> */}
+      <p>Routes works</p>
+      {/* </Router> */}
+    </>
+  );
+};
+
+export default Routes;
